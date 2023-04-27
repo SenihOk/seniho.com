6043bdd8beca831a6580d2ddce2acf62bf493c08
SenihOK <senih.okuyucu@gmail.com>
2023-04-27 15:20:20 -0400

    making really good progress on the backend for the chatroom account creation feature

--- chatroom/app.js
+++ chatroom/app.js
@@ -1,5 +1,48 @@
 const express = require('express');
-const bodyParser = require(body-parser);
-const mysql = require('mysql')
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const { con:db, createUser } = require('./db_connect');
+const bcrypt = require('bcrypt');
 
 const app = express();
+const saltRounds = 10;
+
+// We configure body-parser to parse URL-encoded form data 
+//(the format used by HTML forms). The extended: false option means that it will use the 
+//querystring library to parse the data.
+app.use(bodyParser.urlencoded({extended:false}));
+
+
+//We tell Express to serve static files (like CSS) 
+//from a directory named public located in the same directory 
+//as our app.js file. The __dirname variable contains 
+//the path to the directory where app.js is located.
+app.use(express.static(path.join(__dirname, 'public')));
+
+//defining the /signup endpoint
+app.post('/signup', (req, res) => {
+    const {username, email, password } = req.body;
+
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+    // hashes the passwrod before it is stored in the MySQL database
+        if (err) throw err;
+
+        const query = 'INSERT INTO accounts (username, email, password) VALUES (?,?,?)';
+        createUser(username, email, hash, (err, result) => {
+            if(err) throw err;
+
+            console.log('Account created:', result)
+            res.redirect('/');
+        });
+
+
+    });
+
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,() => {
+    console.log('Server is running on port ${PORT}');
+});
+
--- chatroom/db_connect.js
+++ chatroom/db_connect.js
@@ -1,20 +1,31 @@
 var mysql = require('mysql');
+require('dotenv').config();
 
 var con = mysql.createConnection({
     host: "localhost",
     user: "senih",
-    password: "Senih123",
+    password: process.env.DB_PASSWORD,
     database: "chatroom"
 });
 
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "INSERT INTO accounts (username, password) VALUES ('halil', 'Anne123')";
-    con.query(sql, function (err, result) {
-        if (err) throw err;
-        console.log("user halil added to table");
-        process.exit(0);
-    });
+    // var sql = "INSERT INTO accounts (username, password) VALUES ('halil', 'Anne123')";
+    // con.query(sql, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("user halil added to table");
+    //     process.exit(0);
+    // });
 })
+function createUser(username, email, password, callback) {
+    const query = 'INSERT INTO accounts (username, email, password) VALUES (?,?,?)';
+    con.query(query, [username, email, password], callback);
+
+}
+
+module.exports = {
+    con: con,
+    createUser: createUser,
+};
 
